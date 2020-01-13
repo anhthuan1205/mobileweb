4294d7d9447490573b90e61a76d57f2e4cfe6297
anhthuan95 <thuan.hust.95@gmail.com>
2020-01-13 16:55:11 +0700

    add component

---
+++ mobileweb-FE/src/app/bank-usage-agreement/bank-usage-agreement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-usage-agreement',
+  templateUrl: './bank-usage-agreement.component.html',
+  styleUrls: ['./bank-usage-agreement.component.scss']
+})
+export class BankUsageAgreementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
